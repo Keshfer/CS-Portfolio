da583b5c0fea822a002fc97f5520bc09505c17e0
Keshfer <Keshfur@gmail.com>
2024-06-28 10:17:16 -0700

    Added additional pages

---
+++ index.js
@@ -0,0 +1,13 @@
+function project1() {
+ window.location.href =  "./project1.html";  
+}
+function project2() {
+    window.location.href = "./project2.html";
+}
+function project3() {
+    window.location.href = "./project3.html";
+}
+let resumePage =document.getElementById("resume");
+resumePage.addEventListener("click", function() {
+    window.location.href ="./resume.html";
+});
