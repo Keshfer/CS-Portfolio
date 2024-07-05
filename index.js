768115788c843d467fc70e1024aa03ed0f7a374d
Keshfer <Keshfur@gmail.com>
2024-07-05 15:41:43 -0700

    finished project 1 page

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ function project2() {
 function project3() {
     window.location.href = "./project3.html";
 }
-let resumePage =document.getElementById("resume");
+let resumePage = document.getElementById("resume");
 resumePage.addEventListener("click", function() {
     window.location.href ="./resume.html";
 });
+let projectPage = document.getElementById("projects");
+projectPage.addEventListener("click", function() {
+    window.location.href ="./index.html";
+})
+
+function project1() {
+    window.location.href ="./project1.html";
+}
+function project2() {
+    window.location.href ="./project2.html";
+}
+function project3() {
+    window.location.href ="./project3.html";
+}
---
+++ project1.js
@@ -0,0 +1,8 @@
+let resumePage = document.getElementById("resume");
+resumePage.addEventListener("click", function() {
+    window.location.href = "./resume.html";
+});
+let projectPage = document.getElementById("projects");
+projectPage.addEventListener("click", function () {
+    window.location.href = "./index.html";
+});
